0d104bfb7fc04a5ec35056aded0a15566b67fc03
Bene-Hur Pessoa <benehurp@gmail.com>
2022-02-01 22:14:00 -0300

    Jest collect coverage files

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
    testEnvironment: 'jsdom',
    testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-   collectCoverage: true
+   collectCoverage: true,
+   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx']
 }
